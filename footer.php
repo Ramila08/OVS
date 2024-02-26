ebdb39d78f82aeb6c4f27ee94b2728a38c4f2331
Ramila08 <146409815+Ramila08@users.noreply.github.com>
2024-02-26 04:51:59 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2024. Developed by Rafnas : BIT1510297.
+<br/>
+Online Voting System.
+<br/>
+Final Year Project 2024. Institute of Aiman college
+<br/>
+Contact: <a href="tel:+2609712434">+260 9123456</a> | Email: <a href="mailto:afnanasiya29@gmail.com">afnanasiya29@gmail.com</a>
+
+</footer>
